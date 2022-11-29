87811b1f4d4dd4936c35552432349124d87a9338
luccado <luccadioliveira@gmail.com>
2022-11-29 09:32:09 -0400

    sla oq fiz

--- Assets/Teste.js
+++ Assets/Teste.js
@@ -1,37 +1,65 @@
 let countries = [];
-const countryListElement = document.querySelector("#country-List");
 
-function fetchCountries() {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((response) => response.json())
-      .then((data) => {
-        countries = data.map((x) => x.name.common);
-        countries.sort();
-        loadData(countries, countryListElement);
-    });
+let input = document.getElementById("country-input");
+let ul = document.getElementById("country-list");
+
+// Apenas para pegar os dados
+// É bom fazer funções com funções unicas, essa apenas pegar os dados e retorna
+async function fetchCountries() {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+    return data;
 }
 
-function loadData(data, element) {
-  if (data) {
-    element.innerHTML = "";
-    let innerElement = "";
-    data.forEach((item) => {
-      innerElement += `
-      <li>${item}</li>`;
-    }
-      );
+//  Adicionar o evento de input ao input
+input.addEventListener("input", async (e) => {
+  let inputedWord = e.target.value;
 
-    element.innerHTML = innerElement;
-
+  // Se o input estiver vazio, limpa a lista
+  if(inputedWord.length <= 0) {
+    ul.innerHTML = "";
+    return;
   }
-}
 
-function filterData(data, searchText) {
-  return data.filter((x) => x.toLowerCase().includes(searchText.toLowerCase()));
-}
-fetchCountries();
+  // Se o input estiver vazio, não faz nada
+  if (inputedWord.length > 0) { 
+    // Se o array de países estiver vazio, pega os dados
+    if (countries.length === 0) {
+      countries = await fetchCountries();
+    }
 
-countryInputElement.addEventListener("input", function(){
-  const filteredData = filterData(countries, countryInputElement.value);
-  loadData(filteredData, countryListElement);
-});
+    // Filtra os dados por nome do país
+    // Assim é bom pois retorna todos os dados, alem do nome, caso for utilizar
+    // ATENÇÃO: Ele filtro pode filtrar por mais campos (AO MESMO TEMPO)
+    let filteredCountries = countries.filter((countrie) => {
+      // Alem de filtrar por nomes, vou filtrar por altSpellings, capital e nome mais comum
+      // Destructing
+      const { altSpellings, capital, name: { common }} = countrie;
+      let lowerCaseInputedWorld = inputedWord.toLowerCase();
+      
+      if(altSpellings?.includes(lowerCaseInputedWorld)) {
+        return countrie;
+      }
+      if(capital?.includes(lowerCaseInputedWorld)) {
+        return countrie;
+      }
+      if(common.toLowerCase()?.includes(lowerCaseInputedWorld)) {
+        return countrie;
+      }
+    })
+
+    let ListHTML = "";
+
+    if(filteredCountries.length > 0) {
+      filteredCountries.forEach((item) => {
+        console.log(`item`, item);
+        ListHTML += `
+        <li>${item.name.common}</li>`;
+      })
+    }
+    
+    // Adiciona os dados filtrados na lista
+    ul.innerHTML = ListHTML;
+  }
+
+})
